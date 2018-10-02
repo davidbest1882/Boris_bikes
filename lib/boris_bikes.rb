7a094868b65cf7d1f8b23be23f10ee3a4266d765
David Best <david_best@ntlworld.com>
2018-10-02 17:12:41 +0100

    created test for method working? and created class Bike

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -3,3 +3,10 @@ class DockingStation
 
   end
 end
+
+
+class Bike
+  def working?
+
+  end
+end
--- spec/Boris_bikes_spec.rb
+++ spec/Boris_bikes_spec.rb
@@ -11,3 +11,8 @@ describe DockingStation do
     subject { DockingStation.new }
     it {should respond_to(:release_bike)}
 end
+
+describe Bike do
+  subject { Bike.new }
+  it { should respond_to(:working?) }
+end
