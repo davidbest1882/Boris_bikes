1f97ac6ff74fcd28d9f34c4374c2279b441b3253
Tolvic <terence.h.mace@gmail.com>
2018-10-02 16:39:37 +0100

    created test for release_bike method and added a release_bike DockingStation class

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,3 +1,5 @@
 class DockingStation
+  def release_bike
 
+  end
 end
--- spec/Boris_bikes_spec.rb
+++ spec/Boris_bikes_spec.rb
@@ -1,5 +1,13 @@
 require 'boris_bikes'
+=begin
+describe DockingStation do
+  it 'docking station instances to respond to the method release_bike' do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
+end
+=end
 
 describe DockingStation do
-
+    subject { DockingStation.new }
+    it {should respond_to(:release_bike)}
 end
