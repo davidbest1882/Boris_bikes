e68727a4ec28f5e28ea87ef7ef292ade9d999d19
David Best <david_best@ntlworld.com>
2018-10-03 16:16:14 +0100

    Used attributes and raised errors

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,11 +1,32 @@
 class DockingStation
+  attr_accessor :bike_count
+
+  def initialize
+    @bike_count = 0
+  end
+
   def release_bike
+    if @bike_count > 0
     Bike.new
+    else
+      raise RuntimeError.new("No bikes available")
+    end
+  end
+
+  def dock_bike(bike)
+    bike.docked = true
+    bike.docked
   end
 end
 
 
 class Bike
+  attr_accessor :docked
+
+  def initialize
+    @docked = false
+  end
+
   def working?
     true
   end
--- spec/Boris_bikes_spec.rb
+++ spec/Boris_bikes_spec.rb
@@ -10,12 +10,23 @@ end
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
     it 'gets a bike' do
-      expect(DockingStation.new.release_bike).to be_a(Bike)
+      subject.bike_count = 1
+      expect(subject.release_bike).to be_a(Bike)
       expect(Bike.new.working?).to eq true
+    end
+    it 'release a bike' do
+      expect(Bike.new.docked).to eq false
+    end
+    it 'docks a bike' do
+      bike = Bike.new
+      DockingStation.new.dock_bike(bike)
+      expect(bike.docked).to eq true
+    end
+    it 'release_bike returns error' do
+      expect{subject.release_bike}.to raise_error(RuntimeError, "No bikes available")
     end
 end
 
 describe Bike do
-
   it { is_expected.to respond_to(:working?) }
 end
